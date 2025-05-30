b69b2201dc895c5829de18f44aa6a85fcc2861bd
Brittany Holmes <Brittany.Holmes@vanarts.com>
2025-05-29 18:30:33 -0700

    finished product page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AllProductsComponent } from './all-products/all-products.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
 export const routes: Routes = [
     {path: "home", component: HomeComponent},
-    {path:"allproducts", component: AllProductsComponent}
+    {path:"allproducts", component: AllProductsComponent},
+    {path:"allproducts/:id", component:ProductDetailsComponent}
 ]
